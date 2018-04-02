9ab4b7e892e4e45f66f70bbadc9d551e30c18781
rshawabkeh <alshawabkeh.rami.1987@gmail.com>
2018-04-02 15:34:14 +0300

    ITGCommit

--- src/test/java/NoorProject/EduWaveSafeAndSecurity/GeneralDirectorOfSchoolSecurityAndSafety/EduWaveSafeAndSecurityForms/AddSectionsOfTheForm.java
+++ src/test/java/NoorProject/EduWaveSafeAndSecurity/GeneralDirectorOfSchoolSecurityAndSafety/EduWaveSafeAndSecurityForms/AddSectionsOfTheForm.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Random;
 
 import static NoorProject.Other.NoorLogin.browserQA;
 import static NoorProject.Other.NoorLogin.waitQA;
@@ -31,7 +33,7 @@ public class AddSectionsOfTheForm {
         WebElement UserLabelLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(UserNameLabelLocator));
         UserLabelLocatorWait.click();
 
-        for (int i = 0; i < 6; i++) {
+       // for (int i = 0; i < 3; i++) {
             WebElement FormMainMenuLoactorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(FormMainMenuLoactor));
             FormMainMenuLoactorWait.click();
 
@@ -46,12 +48,45 @@ public class AddSectionsOfTheForm {
             WebElement SerachButtonLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SerachButtonLocator));
             SerachButtonLocatorWait.click();
 
-            List TableSizeLoactor = browserQA.findElements(By.xpath("/html/body/form/div[7]/div[2]/div[2]/div/div/div[3]/div[2]/div/div/table[1]/tbody/tr/td[1]"));
+            By SectionsLinkLocator = By.id("ctl00_PlaceHolderMain_gvForms_ctl02_tdSections");
 
-            int TableSizeInt = TableSizeLoactor.size()-20;
-            System.out.println(TableSizeInt);
+            WebElement SectionsLinkLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SectionsLinkLocator));
+            SectionsLinkLocatorWait.click();
 
-        }
+            List SectionTableList = browserQA.findElements(By.xpath("/html/body/form/div[7]/div[2]/div[2]/div/div/div[1]/div[2]/div/div/table[1]/tbody/tr/td[1]"));
+
+            int TableSize = SectionTableList.size();
+            int TableSizeAdd = SectionTableList.size() + 1;
+            String TableSizeAddFormat = String.format("%02d" , TableSizeAdd);
+
+
+            System.out.println("TableSize::" + TableSize);
+            System.out.println("TableSizeAdd::" + TableSizeAdd);
+            System.out.println("TableSizeAddFormat::" + TableSizeAddFormat);
+
+            By SectionDescrptionSplit = By.id("ctl00_PlaceHolderMain_gvSections_ctl" + TableSizeAddFormat + "_tbAddFormSectionDesc");
+            Random Rand = new Random();
+            int RandomNumber = Rand.nextInt(1000000);
+
+            WebElement SectionDescrptionWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SectionDescrptionSplit));
+
+            SectionDescrptionWait.sendKeys("Rami"+RandomNumber);
+
+            By AddLinkSpilt=By.id("ctl00_PlaceHolderMain_gvSections_ctl"+TableSizeAddFormat+"_lbtnAddFormSectionDesc");
+
+            WebElement AddLinkWait=waitQA.until(ExpectedConditions.visibilityOfElementLocated(AddLinkSpilt));
+            AddLinkWait.click();
+
+
+            By AddSectionsMessageLocator=By.id("ctl00_PlaceHolderMain_lblOpertioanlResult");
+
+            WebElement AddSectionsMessageLocatorWait=waitQA.until(ExpectedConditions.visibilityOfElementLocated(AddSectionsMessageLocator));
+
+            String ActualResult=browserQA.findElement(AddSectionsMessageLocator).getText();
+            String ExcpectedResult="تم إضافة القسم بنجاح.";
+
+            Assert.assertEquals(ActualResult,ExcpectedResult,"لم تتم عملية اضافة القسم بنجاح");
+       // }
 
 
     }
