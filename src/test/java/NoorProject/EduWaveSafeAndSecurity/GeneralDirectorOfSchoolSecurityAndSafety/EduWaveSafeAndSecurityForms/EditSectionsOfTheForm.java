16c3772a2d6ef086efad6db3f2acaca81a303d35
rshawabkeh <alshawabkeh.rami.1987@gmail.com>
2018-04-03 10:21:40 +0300

    ITGCommit

--- src/test/java/NoorProject/EduWaveSafeAndSecurity/GeneralDirectorOfSchoolSecurityAndSafety/EduWaveSafeAndSecurityForms/EditSectionsOfTheForm.java
+++ src/test/java/NoorProject/EduWaveSafeAndSecurity/GeneralDirectorOfSchoolSecurityAndSafety/EduWaveSafeAndSecurityForms/EditSectionsOfTheForm.java
@@ -1,3 +1,7 @@
+/*
+ * Created By Rami Al Shawabkeh 4/3/18 9:55 AM
+ */
+
 package NoorProject.EduWaveSafeAndSecurity.GeneralDirectorOfSchoolSecurityAndSafety.EduWaveSafeAndSecurityForms;
 
 import org.openqa.selenium.By;
@@ -5,16 +9,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.Random;
 
 import static NoorProject.Other.NoorLogin.browserQA;
 import static NoorProject.Other.NoorLogin.waitQA;
 
-public class AddSectionsOfTheForm {
+public class EditSectionsOfTheForm {
 
 
     private By SwitchProfileLocator = By.id("ctl00_oHeader_divSwitchUser");
@@ -23,23 +23,21 @@ public class AddSectionsOfTheForm {
     private By FormStatusLocator = By.id("select2-ctl00_PlaceHolderMain_ddlFormStatus-container");
     private By FormStatusSearchLocator = By.xpath("/html/body/span/span/span[1]/input");
     private By SerachButtonLocator = By.id("ctl00_PlaceHolderMain_ibtnSearch");
+    private By SectionsLinkLocator = By.id("ctl00_PlaceHolderMain_gvForms_ctl02_tdSections");
+    private By EditLinkLocator = By.id("ctl00_PlaceHolderMain_gvSections_ctl02_lbtnEdit");
+    private By SaveLinkLocator = By.id("ctl00_PlaceHolderMain_gvSections_ctl02_lbtnUpdate");
 
 
     @Test
-    public void goToUser() {
+    public void editSectionsOfTheForm() throws InterruptedException {
 
         WebElement SwitchProfileLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SwitchProfileLocator));
         SwitchProfileLocatorWait.click();
 
         WebElement UserLabelLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(UserNameLabelLocator));
         UserLabelLocatorWait.click();
-    }
 
-    @Test
-    public void addSectionsToUnpublishedForm() throws InterruptedException {
-
-        Thread.sleep(1000);
-        // for (int i = 0; i < 3; i++) {
+        //   for (int i = 0; i < 3; i++) {
         WebElement FormMainMenuLoactorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(FormMainMenuLoactor));
         FormMainMenuLoactorWait.click();
 
@@ -54,45 +52,32 @@ public class AddSectionsOfTheForm {
         WebElement SerachButtonLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SerachButtonLocator));
         SerachButtonLocatorWait.click();
 
-        By SectionsLinkLocator = By.id("ctl00_PlaceHolderMain_gvForms_ctl02_tdSections");
 
         WebElement SectionsLinkLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SectionsLinkLocator));
         SectionsLinkLocatorWait.click();
 
-        List SectionTableList = browserQA.findElements(By.xpath("/html/body/form/div[7]/div[2]/div[2]/div/div/div[1]/div[2]/div/div/table[1]/tbody/tr/td[1]"));
-
-        int TableSize = SectionTableList.size();
-        int TableSizeAdd = SectionTableList.size() + 1;
-        String TableSizeAddFormat = String.format("%02d" , TableSizeAdd);
+        AddSectionsOfTheForm Add = new AddSectionsOfTheForm();
+        Add.addSectionsToUnpublishedForm();
 
 
-        System.out.println("TableSize::" + TableSize);
-        System.out.println("TableSizeAdd::" + TableSizeAdd);
-        System.out.println("TableSizeAddFormat::" + TableSizeAddFormat);
-
-        By SectionDescrptionSplit = By.id("ctl00_PlaceHolderMain_gvSections_ctl" + TableSizeAddFormat + "_tbAddFormSectionDesc");
-        Random Rand = new Random();
-        int RandomNumber = Rand.nextInt(1000000);
-
-        WebElement SectionDescrptionWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SectionDescrptionSplit));
-
-        SectionDescrptionWait.sendKeys("Rami" + RandomNumber);
-
-        By AddLinkSpilt = By.id("ctl00_PlaceHolderMain_gvSections_ctl" + TableSizeAddFormat + "_lbtnAddFormSectionDesc");
-
-        WebElement AddLinkWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(AddLinkSpilt));
-        AddLinkWait.click();
+        WebElement EditLinkLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(EditLinkLocator));
+        EditLinkLocatorWait.click();
 
 
-        By AddSectionsMessageLocator = By.id("ctl00_PlaceHolderMain_lblOpertioanlResult");
+        WebElement SaveLinkLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(SaveLinkLocator));
+        SaveLinkLocatorWait.click();
 
-        WebElement AddSectionsMessageLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(AddSectionsMessageLocator));
+        By MessgaeLocator = By.id("ctl00_PlaceHolderMain_lblOpertioanlResult");
 
-        String ActualResult = browserQA.findElement(AddSectionsMessageLocator).getText();
-        String ExcpectedResult = "تم إضافة القسم بنجاح.";
+        WebElement MessgaeLocatorWait = waitQA.until(ExpectedConditions.visibilityOfElementLocated(MessgaeLocator));
 
-        Assert.assertEquals(ActualResult , ExcpectedResult , "لم تتم عملية اضافة القسم بنجاح");
-        // }
+        String ActualResult=browserQA.findElement(MessgaeLocator).getText();
+        String ExcpectedResult="تم تعديل القسم بنجاح.";
+
+        Assert.assertEquals(ActualResult,ExcpectedResult,"لم تتم عملية التعديل بنجاح");
+
+
+        //   }
 
 
     }
